8aa662d7ed4f1bbf0dad31dd7d1f7259e5974ca3
RegisSeki <regisseki@gmail.com>
2016-11-05 20:41:54 +0000

    Usuario agora pode cadastrar uma vaga fora do terminal

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,22 @@
 class JobsController < ApplicationController
   def show
-    @jobs = Job.find(params[:id])
+    @job = Job.find(params[:id])
   end
+  
+  def new
+    @job = Job.new
+  end
+  
+  def create
+    @job = Job.create(jobs_params)
+    redirect_to job_url(@job)
+  end
+  
+  private
+
+  def jobs_params
+    params.require(:job)
+          .permit(:title, :location, :category, :company, :description)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   
-  resources :jobs, only:[:show]
+  resources :jobs, only:[:show, :new, :create]
 
 end
